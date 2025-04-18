7721eb7735e87321f54f911354d11fdaab978117
Prince Biile <biileprinceyennuyar5@gmail.com>
2025-04-18 22:50:13 +0000

    corrected import in the the rootlayout component

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -2,7 +2,8 @@
 import { Outlet } from "react-router-dom";
 
 import Navbar from "./../components/Navbar";
-import Footer from "../components/footer";
+
+import Footer from "./../components/Footer";
 
 const RootLayout = () => {
   return (
